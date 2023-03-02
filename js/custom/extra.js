cfb9b9a84e55d909aecb0c2beb53f5cc689f2014
enes yıldırım <yildirimmenes123@gmail.com>
2023-03-02 17:59:40 +0300

    Initial commit

---
+++ js/custom/extra.js
@@ -0,0 +1,29 @@
+window.addEventListener("DOMContentLoaded",event=>{
+    var navbarMobile=function(){
+        const nCollapsible=document.body.querySelector("#mainNavbar");
+        if(!nCollapsible){
+            return;
+        }
+        if(window.scrollY ===0){
+            nCollapsible.classList.remove("navbar-mobile");
+        }else{
+            nCollapsible.classList.add("navbar-mobile");
+        }
+       
+    };
+    narvarMobile();
+    document.addEventListener("scroll",navbarMobile);
+    const myNavbar=document.body.querySelector("#mainNavbar");
+    if(myNavbar){
+        new bootstrap.ScrollSpy(document.body,{
+            target:"#mainNavbar",
+            offset:74,
+        });
+    }
+});
+var BtnCanvas=document.querySelectorAll(".btn-close-canvas");
+for(let i=0;i<BtnCanvas.length;i++){
+    BtnCanvas[i].addEventListener("click",function(){
+        document.querySelector('[data-bs-dismiss="offcanvas"]').click();
+    });
+}
